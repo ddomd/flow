d0039715e6af9c88ddbfbf4f9acc14a79febad00
ddomd <davidedominicibiz@gmail.com>
2023-09-26 20:21:46 +0200

    changed axios bootstrap

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -7,7 +7,7 @@
 import axios from 'axios';
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpsRequest';
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
